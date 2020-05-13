8e5789fa1ff88acb3b5f87b8cc2bc77cb54ae30d
bbasung1 <59134394+bbasung1@users.noreply.github.com>
2020-05-13 19:31:06 +0900

    변수선언 밖으로 뺌
    lj의 요청

--- githubtest.cpp
+++ githubtest.cpp
@@ -10,15 +10,15 @@ int main(void)
     int raw[1000][7];//rww data 임시코드. 이것을 포함한 모든 변수는class 정해지면 삭제 또는 수정 예정
     printf("모드 입력:");
     scanf("%d",&a);
+    int temp1[45];//(보너스 포함)숫자가 나온 횟수
+    int temp2[45];//편차
     switch(a)
     {
       case 1:
-        int temp1[45];//(보너스 포함)숫자가 나온 횟수
         for(int i=0;i<45;i++)
         {
           temp1[i]=std::count(*raw,*(raw+700),i+1);
         }
-        int temp2[45];//편차
         for(int i=0;i<45;i++)
         {
             temp2[i]=temp1[i]- 910*7/45;
@@ -34,8 +34,9 @@ int main(void)
         break;
 
       default:
-        printf("잘못된 명령어입니다.");
+        printf("잘못된 명령어입니다.\n");
         break;
+1
 
     }
   }
