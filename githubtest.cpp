71ce7beb609ac3c6d848644167d0fd0319d7b5bf
bbasung1 <59134394+bbasung1@users.noreply.github.com>
2020-05-13 18:33:41 +0900

    (단순 확률모드에서)count 및 편차 구현

--- githubtest.cpp
+++ githubtest.cpp
@@ -1,16 +1,28 @@
-#include "stdio.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 int main(void)
 {
   int a;
   while(1)
   {
+    int raw[1000][7];//rww data 임시코드. 이것을 포함한 모든 변수는class 정해지면 삭제 또는 수정 예정
     printf("모드 입력:");
     scanf("%d",&a);
     switch(a)
     {
       case 1:
-      
+        int temp1[45];//(보너스 포함)숫자가 나온 횟수
+        for(int i=0;i<45;i++)
+        {
+          temp1[i]=std::count(*raw,*(raw+700),i+1);
+        }
+        int temp2[45];//편차
+        for(int i=0;i<45;i++)
+        {
+            temp2[i]=temp1[i]- 910*7/45;
+        }
       case 2:
 
       case 3:
@@ -18,7 +30,7 @@ int main(void)
       case 4:
 
       default:
-        printf("잘못된 명령어입니다.")
+        printf("잘못된 명령어입니다.");
 
     }
   }
