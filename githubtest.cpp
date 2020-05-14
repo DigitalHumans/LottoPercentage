a7ab9f13357834a473c71b0af792ee7783c55002
bbasung1 <59134394+bbasung1@users.noreply.github.com>
2020-05-14 23:40:28 +0900

    단순 확률 기반 모드 구현
    해당 코드는 비효율적이라 판단, 점진적으로 최적화 예정

--- githubtest.cpp
+++ githubtest.cpp
@@ -11,7 +11,9 @@ int main(void)
     printf("모드 입력:");
     scanf("%d",&a);
     int temp1[45];//(보너스 포함)숫자가 나온 횟수
-    int temp2[45];//편차
+    float temp2[45];//편차
+    float temp3[45];//편차 sort
+    float temp4[45];//많이 나오는 빈도수
     switch(a)
     {
       case 1:
@@ -23,13 +25,27 @@ int main(void)
         {
             temp2[i]=temp1[i]- 910*7/45;
         }
-        sort(temp2.begin(),temp2.end());
-        printf("추천 번호는");
+        for(int i=0;i<45;i++)
+        {
+            temp3[i]=temp2[i];
+        }
+        sort(temp3.begin(),temp3.end());
+        for(int i=0;i<45;i++)
+        {
+            for(int k=0;k<45;k++)
+            {
+              if(temp3[i]==temp2[k])
+              {
+                temp4[i]==k+1;
+              }
+            }
+        }
+        printf("추천 로또 번호는 ");
         for(int i=0;i<7;i++)
         {
-          printf("%d",temp2[i]);
+          printf("%lf",temp4[i]);
         }
-        printf("입니다");
+        printf("입니다.")
         break;
       case 2:
           break;
