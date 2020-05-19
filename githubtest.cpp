0461f34a08f502e949c5c0a6ab7c8d76c4241296
LJLee37 <ljlee3759@gmail.com>
2020-05-19 20:44:50 +0900

    일단 대충 조금 해봄.

--- githubtest.cpp
+++ githubtest.cpp
@@ -32,7 +32,7 @@ int main(void)
   int a;
   while(1)
   {
-    int raw[1000][7];//rww data 임시코드. 이것을 포함한 모든 변수는class 정해지면 삭제 또는 수정 예정
+        lottoData_t lottoData;
 	cout << "모드 입력 : ";
 	cin >> a;
     int temp1[45];//(보너스 포함)숫자가 나온 횟수
@@ -41,6 +41,7 @@ int main(void)
     switch(a)
     {
       case 1:
+	  lottoData.normal_predict(/*하핫.*/);
         for(int i=0;i<45;i++)
         {
           temp1[i]=std::count(*raw,*(raw+700),i+1);
