0e44a3dada8b6e5479e4db58932a3348f43485e3
LJLee37 <ljlee3759@gmail.com>
2020-05-20 12:44:43 +0900

    엑셀 읽기 작업 끝.
    normal_predic에 옮김.
    수정작업 좀 필요할거임.
    
    인덴테이션 통일함. 한 번의 인덴트당 4개의 공백.
    data.xlsx에 있는 데이터가 갑자기 적어져서 당황하지 말 것. 원본 데이터는 구글에 살아있음. 읽기 편하게 하려고 날데이터만 남김.
    그 날데이터가 그대로 lottoData_t의 rawData로 들어갔으니 잘 이용할 것.

--- githubtest.cpp
+++ githubtest.cpp
@@ -7,71 +7,73 @@ using namespace std;
 class lottoData_t
 {
 private:
-    vector<vector<int>> rawData;
-
+    vector<vector<int>> rawData;//rawData[n][6] : 보너스번호, rawData[n][0] ~ rawData[n][5] : 당첨번호
 public:
     auto normal_predict(int startRange, int endRange)//범위에 따라 단순 예상
     {
-	//기범이 파이팅
+        //기범이 파이팅
+        vector<pair<float, int> >temp3;//편차 sort
+        for(int i=0;i<45;i++)
+        {
+        // temp1[i]=std::count(*raw,*(raw+700),i+1);
+        // printf("%d",temp1[i]);
+        }
+        for(int i=0;i<45;i++)
+            temp3.push_back(make_pair(temp1[i],i+1));
+        sort(temp3.begin(),temp3.end());
+        reverse(temp3.begin(), temp3.end());
+        vector<int> retval;
+        for (auto i = 0; i < 6; i++)
+            retval.push_back(temp3[i].second);
+        sort(retval.begin(), retval.end());
+        return retval;
     }
     auto pattern_predict(int startRange, int endRange)//패턴인식 예상
     {
-	//성우 파이팅
+        //성우 파이팅
     }
-    lottoData_t()
+    lottoData_t(string path = "./data.xlsx")
     {
-	//쩨 파이팅
-    }
-    lottoData_t(string path)
-    {
-	//쩨 파이팅
+        xlnt::workbook wb;
+        wb.load(path);
+        auto ws = wb.active_sheet();
+        for (auto row : ws.rows())
+        {
+            vector<int> temp;
+            for (auto cell : row)
+                temp.push_back(stoi(cell.to_string()));
+            rawData.push_back(temp);
+        }
     }
 };
 
 int main(void)
 {
-  int a;
-  while(1)
-  {
-        lottoData_t lottoData;
-	cout << "모드 입력 : ";
-	cin >> a;
-    int temp1[45];//(보너스 포함)숫자가 나온 횟수
-    vector <pair<int, float> >temp3;//편차 sort
-    
-    switch(a)
+    int a;
+    while(1)
     {
-      case 1:
-	  lottoData.normal_predict(/*하핫.*/);
-        for(int i=0;i<45;i++)
+        lottoData_t lottoData;
+        cout << "모드 입력 : ";
+        cin >> a;
+        vector<int>temp;
+        switch(a)
         {
-          temp1[i]=std::count(*raw,*(raw+700),i+1);
-          printf("%d",temp1[i]);
+        case 1:
+            temp = lottoData.normal_predict(1, 911);
+            cout << "추천 로또 번호는 ";
+            for(auto i : temp)
+              cout << i << " ";
+            cout << "입니다." << endl;
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            cout << "잘못된 명령어입니다." << endl;
+            break;
         }
-       
-        for(int i=0;i<45;i++)
-          temp3.push_back(pair<float, int>(temp1[i],i+1));
-        
-        sort(temp3.begin(),temp3.end());
-        
-        cout << "추천 로또 번호는 ";
-        for(int i=44;i>=38;i--)
-          cout << temp3[i].second <<endl;
-        cout << "입니다." << endl;
-        break;
-      case 2:
-          break;
-
-      case 3:
-        break;
-
-      case 4:
-        break;
-
-      default:
-        cout << "잘못된 명령어입니다." << endl;
-        break;
-
     }
-  }
 }
